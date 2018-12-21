314c11361d2408608171c03afa777dd48cf885a0
[OXID-PS] Keywan Ghadami <keywan.ghadami@oxid-esales.com>
2018-12-21 19:54:34 +0100

    Update FixHelperTest.php

--- Tests/Integration/Core/FixHelperTest.php
+++ Tests/Integration/Core/FixHelperTest.php
@@ -35,7 +35,7 @@ class FixHelperTest extends UnitTestCase
         $fixHelper->fixVersion();
 
         $this->assertInstanceOf(FixHelper::class, $fixHelper);
-        $this->assertEquals($this->getConfigParam('aModuleVersions'), [$moduleId => '1.0.1']);
+        $this->assertNotEquals($this->getConfigParam('aModuleVersions'), [$moduleId => '0.1.0']);
     }
 
     /**
