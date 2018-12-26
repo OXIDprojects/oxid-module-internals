8c58867d7894b062d57b9c0812607c941908de71
[OXID-PS] Keywan Ghadami <keywan.ghadami@oxid-esales.com>
2018-12-26 12:27:57 +0100

    Update .travis.before_script.sh

--- .travis.before_script.sh
+++ .travis.before_script.sh
@@ -23,6 +23,8 @@ sed -i -e "s@<dbPwd>@@g" source/config.inc.php
 sed -i -e "s@<sShopURL>@http://127.0.0.1@g" source/config.inc.php
 sed -i -e "s@<sShopDir>@/home/travis/OXID/source@g" source/config.inc.php
 sed -i -e "s@<sCompileDir>@/home/travis/OXID/source/tmp@g" source/config.inc.php
+sed -i "s|\$this->edition = ''|\$this->edition = 'CE'|" source/config.inc.php
+
 sed -i -e "s@partial_module_paths: null@partial_module_paths: oxcom/moduleinternals@g" test_config.yml
 sed -i -e "s@run_tests_for_shop: true@run_tests_for_shop: false@g" test_config.yml
 
