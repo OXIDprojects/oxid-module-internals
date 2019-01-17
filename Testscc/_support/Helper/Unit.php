63bffde87d6af04b585ddf0af293d06b228647f6
Roland Golla <rolandgolla@gmail.com>
2019-01-17 15:10:00 +0100

    initial setup from other develop state

---
+++ Testscc/_support/Helper/Unit.php
@@ -0,0 +1,10 @@
+<?php
+namespace OxidCommunity\ModuleInternals\Testcc\Helper;
+
+// here you can define custom actions
+// all public methods declared in helper class will be available in $I
+
+class Unit extends \Codeception\Module
+{
+
+}
