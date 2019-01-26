94d9189ce9a9eaddd77ee9462e67ace9eee6c155
Keywan Ghadami <keywan.ghadami@oxid-esales.com>
2019-01-26 22:21:38 +0100

    configure shop when running tests

--- prepare.sh
+++ prepare.sh
@@ -1,4 +1,5 @@
 : "${OXID:=6.1}"
+echo $OXID
 docker swarm init
 docker stack deploy --compose-file docker-compose.yml oxid
-docker run --network=oxid_proxy -ti keywanghadamioxid/php-apache-full:${OXID} bash ../build.sh
+docker run --network=oxid_proxy -ti keywanghadamioxid/php-apache-full:$OXID bash ../build.sh
--- test.sh
+++ test.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 cd /var/www/source
-sed -i -e "s@partial_module_paths: null@partial_module_paths: oxcom/moduleinternals@g" test_config.yml
-sed -i -e "s@run_tests_for_shop: true@run_tests_for_shop: false@g" test_config.yml
+sed -i -e "s@<dbHost>@oxid6_mysql@g; s@<dbName>@oxid@g; s@<dbUser>@oxid@g; s@<dbPwd>@oxid@g; s@<sShopURL>@http://oxid6_apache/@g; s@<sShopDir>@/var/www/source@" config.inc.php
+
+sed -i -e "s@partial_module_paths: null@partial_module_paths: oxcom/moduleinternals@g; s@run_tests_for_shop: true@run_tests_for_shop: false@g" test_config.yml
 
 composer config minimum-stability dev
 
