0354a73ddde58c3eef402192618e1a221b637e95
Keywan Ghadami <keywan.ghadami@oxid-esales.com>
2019-01-26 15:42:02 +0100

    user prebuild container - using a docker container with preinstalled oxid because it makes things faster and simpler

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+sed -i -e "s@partial_module_paths: null@partial_module_paths: oxcom/moduleinternals@g" test_config.yml
+sed -i -e "s@run_tests_for_shop: true@run_tests_for_shop: false@g" test_config.yml
+
+composer config minimum-stability dev
+
+#Module Registrieren
+composer clear-cache
+composer config repo.packagist false
+composer config repositories.travis path /module
+
+composer require "oxid-community/moduleinternals:*"
