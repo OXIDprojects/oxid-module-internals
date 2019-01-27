22996315678c62c986e375655f035f22caae9043
Keywan Ghadami <keywan.ghadami@oxid-esales.com>
2019-01-27 11:07:06 +0100

    call test.sh

--- prepare.sh
+++ prepare.sh
@@ -2,4 +2,4 @@
 echo $OXID
 docker swarm init
 docker stack deploy --compose-file docker-compose.yml oxid
-docker run --network=oxid_proxy -ti keywanghadamioxid/php-apache-full:$OXID bash ../build.sh
+docker run --network=oxid_proxy -ti keywanghadamioxid/php-apache-full:$OXID bash /module/test.sh
