fee2de89162903c95cddf9fae4a5f499b5992582
[OXID-PS] Keywan Ghadami <keywan.ghadami@oxid-esales.com>
2019-01-29 14:44:01 +0100

    merge commands, and output

--- .travis.before_script.sh
+++ .travis.before_script.sh
@@ -4,16 +4,11 @@ cd ~/
 mkdir OXID
 cd OXID
 composer create-project oxid-esales/oxideshop-project . dev-b-${OXID}-ce
-sed -i -e "s@<dbHost>@127.0.0.1@g" source/config.inc.php
-sed -i -e "s@<dbName>@oxid@g" source/config.inc.php
-sed -i -e "s@<dbUser>@root@g" source/config.inc.php
-sed -i -e "s@<dbPwd>@@g" source/config.inc.php
-sed -i -e "s@<sShopURL>@http://127.0.0.1@g" source/config.inc.php
-sed -i -e "s@<sShopDir>@/home/travis/OXID/source@g" source/config.inc.php
-sed -i -e "s@<sCompileDir>@/home/travis/OXID/source/tmp@g" source/config.inc.php
+sed -i -e "s@<dbHost>@127.0.0.1@g; s@<dbName>@oxid@g; s@<dbUser>@root@g; s@<dbPwd>@@g; s@<sShopURL>@http://127.0.0.1@g" source/config.inc.php
+sed -i -e "s@<sShopDir>@/home/travis/OXID/source@g; s@<sCompileDir>@/home/travis/OXID/source/tmp@g" source/config.inc.php
 sed -i -e "s@partial_module_paths: null@partial_module_paths: oxcom/moduleinternals@g" test_config.yml
 sed -i -e "s@run_tests_for_shop: true@run_tests_for_shop: false@g" test_config.yml
-
+cat test_config.yml
 composer config minimum-stability dev
 
 #Module Registrieren
