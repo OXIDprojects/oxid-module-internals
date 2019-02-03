3682a17a0cff450820dabbc7b9314b9a3ade7a31
Keywan Ghadami <keywan.ghadami@oxid-esales.com>
2019-02-03 22:42:00 +0100

    some configuration changes

--- ci/install.sh
+++ ci/install.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
+mkdir -p /module/ci/www
 cd /module/ci/www
 composer create-project oxid-esales/oxideshop-project . dev-b-6.0-ce
 
-cd /var/www/source
+cd source
 sed -i -e "s@<dbHost>@oxid6_mysql@g; s@<dbName>@oxid@g; s@<dbUser>@oxid@g; s@<dbPwd>@oxid@g; s@<sShopURL>@http://oxid6_apache/@g; s@<sShopDir>@/var/www/source@" config.inc.php
 
 sed -i -e "s@partial_module_paths: null@partial_module_paths: oxcom/moduleinternals@g; s@run_tests_for_shop: true@run_tests_for_shop: false@g" test_config.yml
@@ -18,3 +19,4 @@ composer config repositories.travis path /module
 composer require "oxid-community/moduleinternals:*"
 
 ./vendor/bin/runtests
+
--- ci/prepare.sh
+++ ci/prepare.sh
@@ -3,4 +3,5 @@
 echo $OXID
 docker swarm init
 docker stack deploy --compose-file ci/docker-stack.yml oxid
-ci/execOnFpm.sh /module/ci/install.sh
+sleep 1
+ci/execOnFpm.sh bash /module/ci/install.sh
