78d189cbbc7649d57be158031241389f7b55787a
Keywan Ghadami <keywan.ghadami@oxid-esales.com>
2019-02-04 14:59:29 +0100

    fix config.inc.php

--- ci/install.sh
+++ ci/install.sh
@@ -3,10 +3,9 @@ COMPOSER_NO_INTERACTION=1
 cd /ci/oxideshop
 composer install
 
-sed -i -e "s@<dbHost>@mysql@g; s@<dbName>@oxid@g; s@<dbUser>@oxid@g; s@<dbPwd>@oxid@g; s@<sShopURL>@http://oxid6_apache/@g; s@<sShopDir>@/var/www/source@" source/config.inc.php
+sed -i -e "s@<dbHost>@mysql@g; s@<dbName>@oxid@g; s@<dbUser>@oxid@g; s@<dbPwd>@oxid@g; s@<sShopURL>@http://oxid6_apache/@g; s@<sShopDir>@/ci/oxideshop/source@; s@<sCompileDir>@/ci/oxideshop/source/tmp@" source/config.inc.php
 
 sed -i -e "s@partial_module_paths: null@partial_module_paths: oxcom/moduleinternals@g; s@run_tests_for_shop: true@run_tests_for_shop: false@g" test_config.yml
-
 
 ./vendor/bin/runtests
 
