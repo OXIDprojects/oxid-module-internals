2e16639c141cd289a6be155309acafccb86ac253
Keywan Ghadami <keywan.ghadami@oxid-esales.com>
2019-02-05 14:42:11 +0100

    add logs, allow root user access for apache container

--- ci/rootOnHttpd.sh
+++ ci/rootOnHttpd.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker exec -e TERM=$TERM -ti oxid_apache.1.$(docker service ps -f 'name=oxid_apache.1' -f 'desired-state=Running' oxid_apache -q --no-trunc) "$@"
+docker exec -e TERM=$TERM --user "0" -ti oxid_apache.1.$(docker service ps -f 'name=oxid_apache.1' -f 'desired-state=Running' oxid_apache -q --no-trunc) "$@"
