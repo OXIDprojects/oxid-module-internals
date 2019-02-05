1bdad42767b2ba0bc78d1dbabaa1af687fec3d42
Keywan Ghadami <keywan.ghadami@oxid-esales.com>
2019-02-05 16:15:55 +0100

    fix folders in fcgi proxy setting and oxid config

--- ci/install.sh
+++ ci/install.sh
@@ -3,7 +3,7 @@ COMPOSER_NO_INTERACTION=1
 cd /var/www/oxideshop/source
 composer install
 
-sed -i -e "s@<dbHost>@mysql@g; s@<dbName>@oxid@g; s@<dbUser>@oxid@g; s@<dbPwd>@oxid@g; s@<sShopURL>@http://oxid6_apache/@g; s@<sShopDir>@/ci/oxideshop/source@; s@<sCompileDir>@/ci/oxideshop/source/tmp@" source/config.inc.php
+sed -i -e "s@<dbHost>@mysql@g; s@<dbName>@oxid@g; s@<dbUser>@oxid@g; s@<dbPwd>@oxid@g; s@<sShopURL>@"http://".$_SERVER['HTTP_HOST']."/"@g; s@<sShopDir>@__DIR__@; s@<sCompileDir>@__DIR__ . '/tmp'@" source/config.inc.php
 
 sed -i -e "s@partial_module_paths: null@partial_module_paths: oxcom/moduleinternals@g; s@run_tests_for_shop: true@run_tests_for_shop: false@g" test_config.yml
 
