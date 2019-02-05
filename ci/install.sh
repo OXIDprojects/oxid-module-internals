cba2e97b2b05e6875dcdb8fbf05c1a8ce4cefbe6
Keywan Ghadami <keywan.ghadami@oxid-esales.com>
2019-02-05 17:14:05 +0100

    upgrade testing library to version 6 because newer dependencies will be needed for codeception

--- ci/install.sh
+++ ci/install.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 COMPOSER_NO_INTERACTION=1
-cd /var/www/oxideshop/source
+cd /var/www/oxideshop/
+rm -rf source
 composer install
-
-sed -i -e "s@<dbHost>@mysql@g; s@<dbName>@oxid@g; s@<dbUser>@oxid@g; s@<dbPwd>@oxid@g; s@<sShopURL>@"http://".$_SERVER['HTTP_HOST']."/"@g; s@<sShopDir>@__DIR__@; s@<sCompileDir>@__DIR__ . '/tmp'@" source/config.inc.php
-
+touch source/.gitignore
+sed -i -e "s@<dbHost>@mysql@g; s@<dbName>@oxid@g; s@<dbUser>@oxid@g; s@<dbPwd>@oxid@g; s@'<sShopURL>'@'http://'.\$_SERVER['HTTP_HOST']@g; s@'<sShopDir>'@__DIR__@; s@'<sCompileDir>'@__DIR__ . '/tmp'@" source/config.inc.php
 sed -i -e "s@partial_module_paths: null@partial_module_paths: oxcom/moduleinternals@g; s@run_tests_for_shop: true@run_tests_for_shop: false@g" test_config.yml
 
 ./vendor/bin/runtests
