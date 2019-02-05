4c1dc4ba144abbeb0df8d0e04ab249189cb5a8dd
Keywan Ghadami <keywan.ghadami@oxid-esales.com>
2019-02-05 11:03:48 +0100

    use the running fmp container for shell access - filter stale containers when using execOnFmp script - remove host user from httpd shell because its not useful - change document directory to debug httpd (still not working)

--- ci/execOnFpm.sh
+++ ci/execOnFpm.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 #shift 1
-docker exec -e TERM=$TERM --user "${UID}:${GID}" -ti oxid_fpm.1.$(docker service ps -f 'name=oxid_fpm.1'  oxid_fpm -q --no-trunc) "$@"
+docker exec -e TERM=$TERM --user "${UID}:${GID}" -ti oxid_fpm.1.$(docker service ps -f 'name=oxid_fpm.1'  -f 'desired-state=Running' oxid_fpm -q --no-trunc|head -n 1) "$@"
