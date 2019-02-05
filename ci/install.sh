6b708bbdb83755c29bf8baab722579bb5fd84b72
Keywan Ghadami <keywan.ghadami@oxid-esales.com>
2019-02-05 15:39:28 +0100

    using /var/www as base as developers maybe used to that folder

--- ci/install.sh
+++ ci/install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 COMPOSER_NO_INTERACTION=1
-cd /ci/oxideshop
+cd /var/www/oxideshop/source
 composer install
 
 sed -i -e "s@<dbHost>@mysql@g; s@<dbName>@oxid@g; s@<dbUser>@oxid@g; s@<dbPwd>@oxid@g; s@<sShopURL>@http://oxid6_apache/@g; s@<sShopDir>@/ci/oxideshop/source@; s@<sCompileDir>@/ci/oxideshop/source/tmp@" source/config.inc.php
