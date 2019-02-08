d190fd494a8798393f23435111ba578980d051a0
Keywan Ghadami <keywan.ghadami@oxid-esales.com>
2019-02-08 17:04:48 +0100

    try running docker compose in detached mode on travis

--- ci/run_stack.sh
+++ ci/run_stack.sh
@@ -10,5 +10,5 @@ export GID="$(id -g $(whoami))"
 #docker stack deploy --compose-file ci/docker-stack.yml oxid
 cd "$(dirname "$0")"
 docker-compose --version
-docker-compose up&
+docker-compose -d up
 export DOCKER_COMPOSE_PID=$!
