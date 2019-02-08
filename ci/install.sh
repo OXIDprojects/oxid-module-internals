1a696f730ec40029864d3d64d97c27e623fa8af7
Keywan Ghadami <keywan.ghadami@oxid-esales.com>
2019-02-08 15:50:57 +0100

    restore git keep

--- ci/install.sh
+++ ci/install.sh
@@ -4,7 +4,7 @@ COMPOSER_NO_INTERACTION=1
 cd /ci/oxideshop/
 rm -rf source
 composer install
-touch source/.gitignore
+touch source/.gitkeep
 sed -i -e "s@<dbHost>@mysql@g; s@<dbName>@oxid@g; s@<dbUser>@oxid@g; s@<dbPwd>@oxid@g; s@'<sShopURL>'@'http://'.\$_SERVER['HTTP_HOST']@g; s@'<sShopDir>'@__DIR__@; s@'<sCompileDir>'@__DIR__ . '/tmp'@" source/config.inc.php
 sed -i -e "s@partial_module_paths: null@partial_module_paths: oxcom/moduleinternals@g; s@run_tests_for_shop: true@run_tests_for_shop: false@g" test_config.yml
 
