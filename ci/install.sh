72d61ffefe36e82948e56bf21e222af21e13affd
Keywan Ghadami <keywan.ghadami@oxid-esales.com>
2019-02-08 17:28:16 +0100

    do not call test twice

--- ci/install.sh
+++ ci/install.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -e
+set -x
 COMPOSER_NO_INTERACTION=1
 cd /ci/oxideshop/
 rm -rf source
@@ -7,6 +8,3 @@ composer install
 touch source/.gitkeep
 sed -i -e "s@<dbHost>@mysql@g; s@<dbName>@oxid@g; s@<dbUser>@oxid@g; s@<dbPwd>@oxid@g; s@'<sShopURL>'@'http://'.\$_SERVER['HTTP_HOST']@g; s@'<sShopDir>'@__DIR__@; s@'<sCompileDir>'@__DIR__ . '/tmp'@" source/config.inc.php
 sed -i -e "s@partial_module_paths: null@partial_module_paths: oxcom/moduleinternals@g; s@run_tests_for_shop: true@run_tests_for_shop: false@g" test_config.yml
-
-./vendor/bin/runtests
-
