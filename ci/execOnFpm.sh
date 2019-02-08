11c0572a7d66de5c8a6af7243013c7d976497994
Keywan Ghadami <keywan.ghadami@oxid-esales.com>
2019-02-08 14:42:02 +0100

    run fpm install script in the right directory

--- ci/execOnFpm.sh
+++ ci/execOnFpm.sh
@@ -4,4 +4,5 @@ set -x
 #shift 1
 GID="$(id -g $(whoami))"
 #docker exec -e TERM=$TERM --user "${UID}:${GID}" -ti oxid_fpm.1.$(docker service ps -f 'name=oxid_fpm.1'  -f 'desired-state=Running' oxid_fpm -q --no-trunc|head -n 1) "$@"
+cd "$(dirname "$0")"
 docker-compose exec -e TERM=$TERM  fpm "$@"
