9d527998cc23eef9d5566a36da2a0012e83fad51
Keywan Ghadami <keywan.ghadami@oxid-esales.com>
2019-02-08 10:57:06 +0100

    try to get container id

--- ci/prepare.sh
+++ ci/prepare.sh
@@ -2,6 +2,11 @@
 ./ci/run_stack.sh
 sleep 2
 docker stack ps --no-trunc oxid
+docker ps
+containerName=oxid_fpm.1.$(docker service ps -f 'name=oxid_fpm.1'  -f 'desired-state=Running' oxid_fpm -q --no-trunc|head -n 1);
+containerId=$(docker inspect --format="{{.ID}}" $containerName)
+docker exec -ti $containerId pwd
+
 ./ci/execOnFpm.sh bash /module/ci/install.sh
 docker stack ps --no-trunc oxid
 #/usr/local/apache2/htdocs
