97d457da89e73734c3ab3157e9ed11ee1692afb6
Keywan Ghadami <keywan.ghadami@oxid-esales.com>
2019-02-08 13:57:46 +0100

    adapt container execution scripts to docker compose

--- ci/execOnFpm.sh
+++ ci/execOnFpm.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 #shift 1
 GID="$(id -g $(whoami))"
-docker exec -e TERM=$TERM --user "${UID}:${GID}" -ti oxid_fpm.1.$(docker service ps -f 'name=oxid_fpm.1'  -f 'desired-state=Running' oxid_fpm -q --no-trunc|head -n 1) "$@"
+#docker exec -e TERM=$TERM --user "${UID}:${GID}" -ti oxid_fpm.1.$(docker service ps -f 'name=oxid_fpm.1'  -f 'desired-state=Running' oxid_fpm -q --no-trunc|head -n 1) "$@"
+docker-compose exec -e TERM=$TERM  fpm "$@"
--- ci/execOnHttpd.sh
+++ ci/execOnHttpd.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 GID="$(id -g $(whoami))"
-docker exec -e TERM=$TERM --user "${UID}:${GID}" -ti oxid_apache.1.$(docker service ps -f 'name=oxid_apache.1' -f 'desired-state=Running' oxid_apache -q --no-trunc) "$@"
+#docker exec -e TERM=$TERM --user "${UID}:${GID}" -ti oxid_apache.1.$(docker service ps -f 'name=oxid_apache.1' -f 'desired-state=Running' oxid_apache -q --no-trunc) "$@"
+docker-compose -e TERM=$TERM exec apache "$@"
