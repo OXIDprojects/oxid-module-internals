70d0c8235e4dff3a4f012e209288ac3162bac0d4
Keywan Ghadami <keywan.ghadami@oxid-esales.com>
2019-02-08 17:26:37 +0100

    fix codeception call

--- ci/test.sh
+++ ci/test.sh
@@ -2,4 +2,4 @@
 cd /ci/oxideshop/source
 php /vendor/bin/runtests
 cd /ci/oxideshop/source/vendor/oxid-community/moduleinternals
-php /ci/oxideshop/source/vendor/bin/codecept run
+php /ci/oxideshop/vendor/bin/codecept run
