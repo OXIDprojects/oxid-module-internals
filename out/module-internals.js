249a96bd6b2bd3a2f059233e27a13ae735d43b1b
Sven Brunk <sven.brunk@oxid-esales.com>
2022-07-26 18:25:43 +0200

    codestyle fixes

--- out/module-internals.js
+++ out/module-internals.js
@@ -2,7 +2,7 @@ var acc = document.getElementsByClassName("accordion");
 var i;
 
 for (i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function() {
+    acc[i].addEventListener("click", function () {
         this.classList.toggle("active");
     });
 }
